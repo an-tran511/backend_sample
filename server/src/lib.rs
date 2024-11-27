b03ae0a4ab1f4049721921b96fb6897449b2d0d5
An <an.tran511@outlook.com>
2024-11-27 20:02:30 +0700

    feat: install and config sea-orm

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,6 +1,18 @@
 use axum::Router;
-use std::net::SocketAddr;
+use def::state::AppState;
+use sea_orm::{ConnectOptions, Database, DatabaseConnection};
+use std::{env, net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+
+async fn connect_db() -> Result<DatabaseConnection, sea_orm::DbErr> {
+    tracing::info!("Connecting to database");
+    let mut opt = ConnectOptions::new(&env::var("DATABASE_URL").unwrap());
+    opt.sqlx_logging(false);
+    let db = Database::connect(opt).await?;
+    Ok(db)
+}
 
 #[tokio::main]
 pub async fn start() {
@@ -9,10 +21,26 @@ pub async fn start() {
         .with_test_writer()
         .init();
 
-    let router = Router::new();
+    let db = match connect_db().await {
+        Ok(db) => {
+            tracing::info!("Connected to database");
+            db
+        }
+        Err(_) => {
+            tracing::error!("Failed to connect to database");
+            std::process::exit(1);
+        }
+    };
+
+    let state = Arc::new(AppState { db });
+
+    let router = Router::new().with_state(state.clone()).layer(
+        TraceLayer::new_for_http().make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO)),
+    );
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let tcp = TcpListener::bind(&addr).await.unwrap();
 
+    tracing::debug!("Listening on {}", addr);
     axum::serve(tcp, router).await.unwrap();
 }
